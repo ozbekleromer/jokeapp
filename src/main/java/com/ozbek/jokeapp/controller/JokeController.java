e99f5f0be516ca779f0bfed4dec017388149ec25
ozbekleromer <omerozbekler94@gmail.com>
2020-11-11 04:23:36 +0100

    Implement controllers, services and template.

---
+++ src/main/java/com/ozbek/jokeapp/controller/JokeController.java
@@ -0,0 +1,30 @@
+package com.ozbek.jokeapp.controller;
+
+import com.ozbek.jokeapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * created by ozbek on 2020-11-11
+ */
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "chucknorris";
+
+    }
+}
---
+++ src/main/java/com/ozbek/jokeapp/services/JokeService.java
@@ -0,0 +1,9 @@
+package com.ozbek.jokeapp.services;
+
+/**
+ * created by ozbek on 2020-11-11
+ */
+public interface JokeService {
+
+    String getJoke();
+}
